95a25878be4e7ae72cab8adcbd935707261d9433
Pravallikasree <74898746+Pravallikasree@users.noreply.github.com>
2020-12-27 22:44:40 +0530

    Add files via upload

---
+++ RandomNumbers.java
@@ -0,0 +1,19 @@
+
+package randomnumbers;
+import java.util.*;
+
+
+public class RandomNumbers {
+
+    
+    public static void main(String[] args)
+    {
+       int counter;
+       Random rnum = new Random();
+       for(counter =1; counter <=10; counter++)
+       {
+           System.out.println(rnum.nextInt(2700));
+       }
+    }
+    
+}
